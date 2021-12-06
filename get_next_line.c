7da0099ec6b81d051d48b5c210b4b450f7874f77
cnysten <nysten.carl@gmail.com>
2021-12-06 20:47:37 +0200

    get_next_line.c static fd array implementation, not working

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 20:29:07 by cnysten           #+#    #+#             */
+/*   Updated: 2021/12/06 20:46:41 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 
@@ -13,33 +25,35 @@ size_t	next_line_size(const char *buf)
 
 int	get_next_line(const int fd, char **line)
 {
-	static char			buf[BUFF_SIZE];
-	static unsigned int	start;
-	static ssize_t		bytes_read;
-	size_t				size;
+	static char	*fds[MAX_FD + 1];
+	ssize_t		bytes_read;
+	size_t		size;
 
-	if (start == 0)
+	if (fd < 0 || *line)
+		return (-1);
+	if (fds[fd])
 	{
-		bytes_read = read(fd, &buf, BUFF_SIZE);
+
+	}
+	else
+	{
+		fds[fd] = (char *) malloc(BUFF_SIZE * sizeof (char)); // 
+		bytes_read = read(fd, fds[fd], BUFF_SIZE);
 		printf("bread: %zd\n", bytes_read);
+		if (bytes_read == 0)
+			return (0);
+		if (bytes_read < 0)
+			return (-1);
+		if (!memchr((void *) fds[fd], '\n', bytes_read)
+				ft_memrealloc();
+		else
+			*line = ft_strnew(bytes_read);
+		if (!*line)
+			return (-1);
+		ft_strncpy(*line, start, size - 1), size - 1);
+		start += size;
+		if (start >= BUFF_SIZE)
+			start = 0;
 	}
-	if (bytes_read == 0)
-	{
-		ft_putendl("EOF");
-		return (0);
-	}
-	if (bytes_read < 0)
-	{
-		ft_putendl("ERROR");
-		return (-1);
-	}
-	size = next_line_size(((const char *)buf + start)) + 1;
-	*line = ft_strnew(size);
-	if (!*line)
-		return (-1);
-	ft_strncpy(*line, ft_strsub((char const *) buf, start, size - 1), size - 1);
-	start += size;
-	if (start >= BUFF_SIZE)
-		start = 0;
 	return (1);
 }
