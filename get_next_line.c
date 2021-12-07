7dd7f30c941581b19602816256197be4573002d8
Carl Nysten <nysten.carl@gmail.com>
2021-12-07 10:22:46 +0200

    get_next_line.c SIGSTOP after last line

--- get_next_line.c
+++ get_next_line.c
@@ -11,9 +11,8 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-size_t	next_line_size(const char *buf)
+size_t	line_len(const char *buf)
 {
 	size_t	i;
 
@@ -23,37 +22,63 @@ size_t	next_line_size(const char *buf)
 	return (i);
 }
 
+size_t	has_newline(const char *buf)
+{
+	size_t	i;
+
+	i = 0;
+	while ((buf)[i] != '\n')
+		i++;
+	return (i);
+}
+
+void	grow_buffer(void **buf, size_t len)
+{
+	void	*new;
+
+	if (!buf || !*buf || len < 1)
+		return ;
+	new = malloc(len + BUFF_SIZE);
+	if (!new)
+		return ;
+	ft_strncpy(new, *buf, len);
+	free(*buf);
+	*buf = new;
+}
+
 int	get_next_line(const int fd, char **line)
 {
 	static char	*fds[MAX_FD + 1];
 	ssize_t		bytes_read;
-	size_t		size;
+	size_t		len;
 
-	if (fd < 0 || *line)
+	if (fd < 0)
 		return (-1);
-	if (fds[fd])
+	if (!fds[fd]) // Check if buffer is currently null
 	{
-
-	}
-	else
-	{
-		fds[fd] = (char *) malloc(BUFF_SIZE * sizeof (char)); // 
+		fds[fd] = (char *) malloc(BUFF_SIZE * sizeof (char));
+		if (!fds[fd])
+			return (-1); // Potential memory leak
 		bytes_read = read(fd, fds[fd], BUFF_SIZE);
-		printf("bread: %zd\n", bytes_read);
-		if (bytes_read == 0)
+		if (bytes_read == 0) // EOF
 			return (0);
-		if (bytes_read < 0)
+		if (bytes_read < 0) // Error
 			return (-1);
-		if (!memchr((void *) fds[fd], '\n', bytes_read)
-				ft_memrealloc();
-		else
-			*line = ft_strnew(bytes_read);
-		if (!*line)
-			return (-1);
-		ft_strncpy(*line, start, size - 1), size - 1);
-		start += size;
-		if (start >= BUFF_SIZE)
-			start = 0;
 	}
+	while (!ft_memchr(fds[fd], '\n', line_len(fds[fd]) + 1))
+	{
+		grow_buffer((void **) &fds[fd], BUFF_SIZE);
+		bytes_read = read(fd, fds[fd], BUFF_SIZE);
+		if (bytes_read == 0) // EOF
+			return (0);
+		if (bytes_read < 0) // Error
+			return (-1);
+	}
+	len = line_len(fds[fd]);
+	*line = ft_strnew(len + 1);
+	if (!*line)
+		return (-1);
+	ft_memcpy((void *) *line, (const void *) fds[fd], len);
+	ft_memmove((void *) fds[fd], (const void *)(fds[fd] + len + 1), BUFF_SIZE);
 	return (1);
 }
